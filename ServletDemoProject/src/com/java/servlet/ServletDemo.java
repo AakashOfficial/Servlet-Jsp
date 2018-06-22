0be424168b63166158f65e80b7f9abf9faf7805d
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-22 19:29:53 +0530

    Project 1 Using Annotations !

--- ServletDemoProject/src/com/java/servlet/ServletDemo.java
+++ ServletDemoProject/src/com/java/servlet/ServletDemo.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/actionurl")
 public class ServletDemo extends HttpServlet {
 
 	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
