a5a40503573708a0e81c478f6b7eced89670bbfd
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-22 19:26:53 +0530

    Project 1 Using XML !

---
+++ ServletDemoProject/WebContent/DemoFile.jsp
@@ -0,0 +1,25 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="actionurl" method="post">
+<table>
+<tr>
+<td>Enter User Name</td>
+<td><input type="text" name="userid" /></td>
+</tr>
+
+<tr>
+<td>Enter Password</td>
+<td><input type="text" name="password" /></td>
+</tr>
+
+<tr>
+<td><input type="submit" name="submitbtn" value="Click" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ ServletDemoProject/src/com/java/servlet/ServletDemo.java
@@ -0,0 +1,18 @@
+package com.java.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/Welcome.jsp");
+		requestDispatcher.forward(req, res);
+	}
+}
