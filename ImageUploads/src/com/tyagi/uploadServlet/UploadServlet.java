0478748041b235e1cb128afcef89c5082aa38e3b
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-10 19:48:55 +0530

    Servlet For Uploadation !

---
+++ ImageUploads/src/com/tyagi/uploadServlet/UploadServlet.java
@@ -0,0 +1,73 @@
+package com.tyagi.uploadServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+import java.sql.*;
+import java.util.zip.*;
+
+@WebServlet("/uploadfile")
+public class UploadServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		String saveFile = "";
+		String contentType = request.getContentType();
+		if ((contentType != null) && (contentType.indexOf("multipart/form-data") >= 0)) {
+			DataInputStream in = new DataInputStream(request.getInputStream());
+			int formDataLength = request.getContentLength();
+			byte dataBytes[] = new byte[formDataLength];
+			int byteRead = 0;
+			int totalBytesRead = 0;
+			while (totalBytesRead < formDataLength) {
+				byteRead = in.read(dataBytes, totalBytesRead, formDataLength);
+				totalBytesRead += byteRead;
+			}
+			String file = new String(dataBytes);
+			saveFile = file.substring(file.indexOf("filename=\"") + 10);
+			saveFile = saveFile.substring(0, saveFile.indexOf("\n"));
+			saveFile = saveFile.substring(saveFile.lastIndexOf("\\") + 1, saveFile.indexOf("\""));
+			int lastIndex = contentType.lastIndexOf("=");
+			String boundary = contentType.substring(lastIndex + 1, contentType.length());
+			int pos;
+			pos = file.indexOf("filename=\"");
+			pos = file.indexOf("\n", pos) + 1;
+			pos = file.indexOf("\n", pos) + 1;
+			pos = file.indexOf("\n", pos) + 1;
+			int boundaryLocation = file.indexOf(boundary, pos) - 4;
+			int startPos = ((file.substring(0, pos)).getBytes()).length;
+			int endPos = ((file.substring(0, boundaryLocation)).getBytes()).length;
+			File ff = new File("C:/UploadedFiles/" + saveFile);
+			FileOutputStream fileOut = new FileOutputStream(ff);
+			fileOut.write(dataBytes, startPos, (endPos - startPos));
+			fileOut.flush();
+			fileOut.close();
+
+			out.print("<table border='2'><tr><td><b>You have successfully upload the file:</b>\r\n" + saveFile
+					+ "</td></tr></table>");
+			Connection connection = null;
+			String connectionURL = "jdbc:h2:tcp://localhost/~/FileUpload";
+			PreparedStatement psmnt = null;
+			try {
+				Class.forName("org.h2.Driver");
+				connection = DriverManager.getConnection(connectionURL, "sa", "test");
+				psmnt = connection.prepareStatement("insert into file1(file_path) values(?)");
+				psmnt.setString(1, ff.getPath());
+				int s = psmnt.executeUpdate();
+				if (s > 0) {
+					System.out.println("Uploaded successfully !");
+				} else {
+					System.out.println("Error!");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
